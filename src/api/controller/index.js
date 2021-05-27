f7de63d37a6542e0ef8bb8fbe9d0bc121ee85dfd
Juan Pablo Acosta <pabloacosta.b17@gmail.com>
2021-05-27 14:28:41 -0500

    Routes controller feature completed

--- src/api/controller/index.js
+++ src/api/controller/index.js
@@ -4,7 +4,7 @@ const router = new Router();
 const connection = require("./../../../database");
 
 router.get("/", (__, res) => {
-  connection.query("SELECT * FROM test", (err, rows, fields) => {
+  connection.query("SELECT * FROM data", (err, rows, fields) => {
     if (!err) {
       res.json(rows);
     } else {
@@ -13,12 +13,49 @@ router.get("/", (__, res) => {
   });
 });
 
-router.get("/countries/:name", (__, res) => {
-  if (infoCities) {
-    res.json(infoCities);
-  } else {
-    res.status(404);
-  }
+router.get("/countries/:name", (req, res) => {
+  const { name } = req.params;
+  connection.query(
+    "SELECT * FROM data WHERE NAME=?",
+    [name],
+    (err, rows, fields) => {
+      if (!err) {
+        res.json(rows);
+      } else {
+        console.log(err);
+      }
+    }
+  );
+});
+
+router.get("/states/:name", (req, res) => {
+  const { name } = req.params;
+  connection.query(
+    "SELECT * FROM data WHERE NAME_STATE=?",
+    [name],
+    (err, rows, fields) => {
+      if (!err) {
+        res.json(rows);
+      } else {
+        console.log(err);
+      }
+    }
+  );
+});
+
+router.get("/cities/:name", (req, res) => {
+  const { name } = req.params;
+  connection.query(
+    "SELECT * FROM data WHERE NAME_CITY=?",
+    [name],
+    (err, rows, fields) => {
+      if (!err) {
+        res.json(rows);
+      } else {
+        console.log(err);
+      }
+    }
+  );
 });
 
 module.exports = router;
